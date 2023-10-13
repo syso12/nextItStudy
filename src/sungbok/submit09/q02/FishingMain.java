21d0990997e0ae194b6d7cdbed3e0eee67c52346
pc32 <ctqwkfhddl@naver.com>
2023-10-13 20:27:04 +0900

    2023-10-13

--- src/sungbok/submit09/q02/FishingMain.java
+++ src/sungbok/submit09/q02/FishingMain.java
@@ -34,12 +34,23 @@ public class FishingMain {
                 // 물고기 잡기 랜덤으로
                 int temp = (int)(Math.random() * fishList.size());
                 System.out.println(fishList.get(temp).getName() + "를 잡았다!!!");
-
+                fishBag.add(fishList.get(temp));
                 // 물고기를 잡아 잡은 고기를 출력
                 // 잡은걸 bag에 저장 (fishbag)
+                // fishbag이라는 리스트에 담는다.(.add)
             }else if(select == 2) {
                 // TODO 잡은 물고기 목록 보기
-                // fiahbag에 있는 물고기와 합산가격 출력
+                // fiahbag에 있는 물고기와 가격 출력
+                // 합산 가격만 따로 출력
+                // 리스트에서 더하기
+                for(int i= 0; i < fishBag.size(); i++){
+                    System.out.println(fishBag.get(i));
+                }
+                int catchfish = 0;
+                for(int i = 0; i < fishBag.size(); i++){
+                   catchfish+=fishBag.get(i).price;
+                }
+                System.out.println("총" + catchfish + "원");
             }else if(select == 3) {
                 // TODO 종료
                 break;
